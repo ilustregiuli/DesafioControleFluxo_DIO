03d3bcfbf436a469eebf19db9a68767049fafba4
Giuli Ilustre <ilustre.giuli@gmail.com>
2025-07-09 12:56:22 -0300

    Solucao do exercicio

---
+++ src/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class ParametrosInvalidosException extends RuntimeException{
+
+    public ParametrosInvalidosException(){
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
